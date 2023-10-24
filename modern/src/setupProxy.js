62c2bdb32de67a441e3c9349a01e520ea4d4aff1
Adam Chilton <adam@dsan.co.uk>
2023-10-24 17:05:41 +0100

    [traccar-tonbridge-latest] remove unused file

--- modern/src/setupProxy.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable import/no-extraneous-dependencies */
-
-const { createProxyMiddleware } = require('http-proxy-middleware');
-
-module.exports = (app) => {
-  app.use(createProxyMiddleware('/api/socket', { target: `wss://${process.env.REACT_APP_URL_NAME}`, ws: true, secure: false }));
-  app.use(createProxyMiddleware('/api', { target: `https://${process.env.REACT_APP_URL_NAME}`, secure: false }));
-};
